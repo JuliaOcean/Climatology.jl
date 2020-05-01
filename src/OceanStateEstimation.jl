b84c939156ee4049708d72942b40bf9d68998563
gaelforget <gforget@mit.edu>
2020-05-01 00:05:44 -0400

    start with get_ecco_files function

--- src/OceanStateEstimation.jl
+++ src/OceanStateEstimation.jl
@@ -1,5 +1,49 @@
 module OceanStateEstimation
 
-greet() = print("Hello World!")
+using CSV, DataFrames, Statistics
+using FortranFiles, MeshArrays, MITgcmTools
+export get_grid_if_needed, get_ecco_files, get_from_dataverse
+
+
+##
+
+"""
+    get_ecco_files(v="oceQnet",t)
+
+```
+using MeshArrays, OceanStateEstimation
+get_grid_if_needed()
+pth="../examples/GRID_LLC90/"
+γ=GridSpec("LatLonCap",pth)
+Γ=GridLoad(γ)
+tmp=get_ecco_files(γ)
+```
+"""
+function get_ecco_files(γ::gcmgrid,v="oceQnet",t=1)
+    pth="../examples/nctiles_climatology/"
+    !isdir("$pth") ? mkdir("$pth") : nothing
+    !isdir("$pth"*v) ? get_from_dataverse(v,pth) : nothing
+    #return read_nctiles("$pth"*"$v/$v","$v",γ,I=(:,:,:,t))
+    return read_nctiles("$pth"*"$v/$v","$v",γ,I=(:,:,t))
+end
+
+function get_from_dataverse(nam::String,pth::String)
+    tmp = CSV.File("nctiles_climatology.csv") |> DataFrame!
+    ii = findall([occursin("$nam", tmp[i,:name]) for i=1:size(tmp,1)])
+    !isdir("$pth"*"$nam") ? mkdir("$pth"*"$nam") : nothing
+    for i in ii
+        ID=tmp[i,:ID]
+        nam1=tmp[i,:name]
+        nam2=joinpath("$pth"*"$nam/",nam1)
+        run(`wget --content-disposition https://dataverse.harvard.edu/api/access/datafile/$ID`);
+        run(`mv $nam1 $nam2`);
+    end
+end
+
+function get_grid_if_needed()
+    if !isdir("../examples/GRID_LLC90")
+        run(`git clone https://github.com/gaelforget/GRID_LLC90 ../examples/GRID_LLC90`)
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
-using OceanStateEstimation
+using OceanStateEstimation, MeshArrays, Statistics
 using Test
 
 @testset "OceanStateEstimation.jl" begin
-    # Write your own tests here.
+    get_grid_if_needed()
+    pth="../examples/GRID_LLC90/"
+    γ=GridSpec("LatLonCap",pth)
+    Γ=GridLoad(γ)
+    tmp=get_ecco_files(γ)
+    tmp=[mean(tmp[j][findall((!isnan).(tmp[j]))]) for j=1:5]
+    ref=[19.88214831145215,47.63055475475805,-44.1122401210416,
+         3.4402271721659816,30.14270126344508]
+    @test tmp==ref
 end
