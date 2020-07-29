a75047c13dbd57537eb510687eaad5674294aa12
gaelforget <gforget@mit.edu>
2020-07-29 17:01:06 -0400

    generalize & add OCCA example

--- src/OceanStateEstimation.jl
+++ src/OceanStateEstimation.jl
@@ -2,33 +2,24 @@ module OceanStateEstimation
 
 using CSV, DataFrames, Statistics
 using FortranFiles, MeshArrays, MITgcmTools
-export get_grid_if_needed, get_ecco_files, get_from_dataverse
-
+export get_from_dataverse, get_ecco_files
 
 ##
 
 """
-    get_ecco_files(v="oceQnet",t)
+    get_from_dataverse(lst::String,nam::String,pth::String)
 
 ```
-using MeshArrays, OceanStateEstimation
-get_grid_if_needed()
-pth="../examples/GRID_LLC90/"
-γ=GridSpec("LatLonCap",pth)
-Γ=GridLoad(γ)
-tmp=get_ecco_files(γ)
+using OceanStateEstimation, CSV, DataFrames
+lst=joinpath(dirname(pathof(OceanStateEstimation)),"../examples/OCCA_climatology.csv")
+pth=joinpath(dirname(pathof(OceanStateEstimation)),"../examples/OCCA_climatology/")
+nams = CSV.File(lst) |> DataFrame!
+nams = nams.name[:]
+[get_from_dataverse(lst,nam,pth) for nam in nams]
 ```
 """
-function get_ecco_files(γ::gcmgrid,v="oceQnet",t=1)
-    pth="../examples/nctiles_climatology/"
-    !isdir("$pth") ? mkdir("$pth") : nothing
-    !isdir("$pth"*v) ? get_from_dataverse(v,pth) : nothing
-    #return read_nctiles("$pth"*"$v/$v","$v",γ,I=(:,:,:,t))
-    return read_nctiles("$pth"*"$v/$v","$v",γ,I=(:,:,t))
-end
-
-function get_from_dataverse(nam::String,pth::String)
-    tmp = CSV.File("../examples/nctiles_climatology.csv") |> DataFrame!
+function get_from_dataverse(lst::String,nam::String,pth::String)
+    tmp = CSV.File(lst) |> DataFrame!
     ii = findall([occursin("$nam", tmp[i,:name]) for i=1:size(tmp,1)])
     !isdir("$pth"*"$nam") ? mkdir("$pth"*"$nam") : nothing
     for i in ii
@@ -40,10 +31,24 @@ function get_from_dataverse(nam::String,pth::String)
     end
 end
 
-function get_grid_if_needed()
-    if !isdir("../examples/GRID_LLC90")
-        run(`git clone https://github.com/gaelforget/GRID_LLC90 ../examples/GRID_LLC90`)
-    end
+get_from_dataverse(nam::String,pth::String) = get_from_dataverse("../examples/nctiles_climatology.csv",nam,pth)
+
+"""
+    get_ecco_files(v="oceQnet",t)
+
+```
+using MeshArrays, OceanStateEstimation
+γ=GridSpec("LatLonCap","./")
+tmp=get_ecco_files(γ,"oceQnet")
+```
+"""
+function get_ecco_files(γ::gcmgrid,v::String,t=1)
+    lst=joinpath(dirname(pathof(OceanStateEstimation)),"../examples/nctiles_climatology.csv")
+    pth=joinpath(dirname(pathof(OceanStateEstimation)),"../examples/nctiles_climatology/")
+    !isdir("$pth") ? mkdir("$pth") : nothing
+    !isdir("$pth"*v) ? get_from_dataverse(lst,v,pth) : nothing
+    #return read_nctiles("$pth"*"$v/$v","$v",γ,I=(:,:,:,t))
+    return read_nctiles("$pth"*"$v/$v","$v",γ,I=(:,:,t))
 end
 
 end # module
