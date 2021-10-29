9776ee53cd95b79d7392b94335f8ad2b8c3e1116
gaelforget <gforget@mit.edu>
2021-10-29 18:35:38 -0400

    add examples/loop_EMD.jl

---
+++ examples/loop_EMD.jl
@@ -0,0 +1,69 @@
+
+@everywhere using Distributed, DistributedArrays, SharedArrays
+@everywhere using OptimalTransport, Statistics, LinearAlgebra
+
+@everywhere function ModToSat(i,j)
+    a=Chl_from_Mod[:,:,i][:]
+    b=Chl_from_Sat[:,:,j][:]
+    a,b=preprocess_Chl(a,b)
+    ε = 0.05
+    sinkhorn2(a,b, Cost, ε)
+end
+
+@everywhere function ModToMod(i,j)
+    a=Chl_from_Mod[:,:,i][:]
+    b=Chl_from_Mod[:,:,j][:]
+    a,b=preprocess_Chl(a,b)
+    ε = 0.05
+    sinkhorn2(a,b, Cost, ε)
+end
+
+@everywhere function SatToSat(i,j)
+    a=Chl_from_Mod[:,:,i][:]
+    b=Chl_from_Mod[:,:,j][:]
+    a,b=preprocess_Chl(a,b)
+    ε = 0.05
+    sinkhorn2(a,b, Cost, ε)
+end
+
+@everywhere include("examples/CBIOMES_climatology_EMD.jl")
+
+II=[[i,j] for i in 1:12, j in 1:12][:];
+
+using Random; JJ=shuffle(II);
+
+#@sync @distributed for k in 
+#    i=II[k][1]
+#    j=II[k][2]
+#    d[i,j]=f1(i,j)
+#end
+
+d = SharedArray{Float64}(12,12)
+t0=[time()]
+for kk in 1:36
+    @sync @distributed for k in (kk-1)*4 .+ collect(1:4)
+     i=JJ[k][1]
+     j=JJ[k][2]
+     d[i,j]=ModToMod(i,j)
+    end
+    dt=time()-t0[1]
+    println("ModToMod $(kk) $(dt)")
+    t0[1]=time()
+    @save "ModToMod_v0.jld2" d;
+end
+
+dd = SharedArray{Float64}(12,12)
+t0=[time()]
+for kk in 1:36
+    @sync @distributed for k in (kk-1)*4 .+ collect(1:4)
+     i=JJ[k][1]
+     j=JJ[k][2]
+     dd[i,j]=ModToMod(i,j)
+    end
+    dt=time()-t0[1]
+    println("ModToMod $(kk) $(dt)")
+    t0[1]=time()
+    @save "SatToSat_v0.jld2" dd;
+end
+
+
