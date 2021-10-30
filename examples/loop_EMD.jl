692320554c5ba55c4edaa80a05fd9db978510fe8
gaelforget <gforget@mit.edu>
2021-10-30 11:11:40 -0400

    add switches

--- examples/loop_EMD.jl
+++ examples/loop_EMD.jl
@@ -1,4 +1,10 @@
 
+calc_SatToSat=true
+calc_ModToMod=false
+
+println(calc_SatToSat)
+println(calc_ModToMod)
+
 @everywhere using Distributed, DistributedArrays, SharedArrays
 @everywhere using OptimalTransport, Statistics, LinearAlgebra
 
@@ -26,7 +32,7 @@ end
     sinkhorn2(a,b, Cost, ε)
 end
 
-@everywhere include("examples/CBIOMES_climatology_EMD.jl")
+@everywhere include("CBIOMES_climatology_EMD.jl")
 
 II=[[i,j] for i in 1:12, j in 1:12][:];
 
@@ -38,6 +44,7 @@ using Random; JJ=shuffle(II);
 #    d[i,j]=f1(i,j)
 #end
 
+if calc_ModToMod
 d = SharedArray{Float64}(12,12)
 t0=[time()]
 for kk in 1:36
@@ -51,19 +58,21 @@ for kk in 1:36
     t0[1]=time()
     @save "ModToMod_v0.jld2" d;
 end
+end
 
+if calc_SatToSat
 dd = SharedArray{Float64}(12,12)
 t0=[time()]
 for kk in 1:36
     @sync @distributed for k in (kk-1)*4 .+ collect(1:4)
      i=JJ[k][1]
      j=JJ[k][2]
-     dd[i,j]=ModToMod(i,j)
+     dd[i,j]=SatToSat(i,j)
     end
     dt=time()-t0[1]
-    println("ModToMod $(kk) $(dt)")
+    println("SatToSat $(kk) $(dt)")
     t0[1]=time()
     @save "SatToSat_v0.jld2" dd;
 end
-
+end
 
