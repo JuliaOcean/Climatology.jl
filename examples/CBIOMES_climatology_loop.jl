aa1fb7be96696661bc8ff0d855aaa256d103ad81
gaelforget <gforget@mit.edu>
2021-10-31 13:32:42 -0400

    add test of methods

--- examples/CBIOMES_climatology_loop.jl
+++ examples/CBIOMES_climatology_loop.jl
@@ -1,12 +1,17 @@
 
 calc_SatToSat=false
-calc_ModToMod=true
+calc_ModToMod=false
+calc_ModToSat=false
+test_methods=true
 
 println(calc_SatToSat)
 println(calc_ModToMod)
+println(calc_ModToSat)
+println(test_methods)
 
 @everywhere using Distributed, DistributedArrays, SharedArrays
 @everywhere using OptimalTransport, Statistics, LinearAlgebra
+@everywhere using Tulip, Distances
 
 @everywhere function ModToSat(i,j)
     a=Chl_from_Mod[:,:,i][:]
@@ -30,6 +35,33 @@ end
     a,b=preprocess_Chl(a,b)
     ε = 0.05
     sinkhorn2(a,b, Cost, ε)
+end
+
+@everywhere function ModToMod_methods(i,j,mthd=1)
+    a=Chl_from_Mod[:,:,i][:]
+    b=Chl_from_Mod[:,:,j][:]
+    a,b=preprocess_Chl(a,b)
+    if mthd==1
+        ε = 0.01
+        sinkhorn2(a,b, Cost, ε)
+    elseif mthd==2
+        ε = 0.01
+        sinkhorn2(a,b, Cost, ε; maxiter=2_000)
+    elseif mthd==3
+        emd2(a,b, Cost, Tulip.Optimizer())
+    elseif mthd==4
+        ε = 0.01
+        γ = quadreg(a,b, Cost, ε; maxiter=100)
+        dot(γ, Cost) #compute optimal cost, directly
+    elseif mthd==5
+        ε = 0.005
+        γ = sinkhorn_stabilized(a,b, Cost, ε; maxiter=5_000)
+        dot(γ, Cost) #compute optimal cost, directly
+    elseif mthd==6
+        ε = 0.005
+        γ = sinkhorn_stabilized_epsscaling(a,b, Cost, ε; maxiter=5_000)
+        dot(γ, Cost) #compute optimal cost, directly
+    end
 end
 
 @everywhere include("CBIOMES_climatology_EMD.jl")
@@ -92,3 +124,25 @@ if calc_ModToSat
     end
 end
     
+
+if test_methods
+    KK=([1,1],[1,2],[1,9])
+    d = SharedArray{Float64}(6,length(KK))
+    t0=[time()]
+    for k in 4:6
+        @sync @distributed for kk in 1:3
+         i=KK[kk][1]
+         j=KK[kk][2]
+         try
+            d[k,kk]=ModToMod_methods(i,j,k)
+         catch
+            d[k,kk]=NaN
+         end
+         println("$(k) $(kk) $(d[k,kk])")
+        end
+        dt=time()-t0[1]
+        println("ModToMod_methods $(k) $(dt)")
+        t0[1]=time()
+        @save "ModToMod_methods_v0.jld2" d;
+    end
+end
