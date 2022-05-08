5ba0829f4c46e27cf528c4b23800a0759e5ce5d6
gaelforget <gforget@mit.edu>
2022-05-08 18:29:52 -0400

    bring up to date

--- examples/ECCO/ECCO_standard_loop.jl
+++ examples/ECCO/ECCO_standard_loop.jl
@@ -1,24 +1,38 @@
 
 using Distributed, SharedArrays, JLD2
 
-@everywhere include("ECCO_pkg_grid_etc.jl")
-@everywhere include("ECCO_standard_analysis.jl")
+#@everywhere include("ECCO_pkg_grid_etc.jl")
+#@everywhere include("ECCO_standard_analysis.jl")
 
-@everywhere sol0="r2"
-@everywhere sol="ECCOv4"*sol0*"_analysis"
+@everywhere begin
+    using OceanStateEstimation, Printf, TOML, MeshArrays
+
+    γ,Γ,LC=ECCO_helper_functions.GridLoad_Main()
+    nr=length(Γ.DRF)
+    nl=length(LC)
+
+    sol0="r2"
+    sol="ECCOv4"*sol0*"_analysis" 
+    
+    fil=joinpath(pth,sol,"ECCO_standard_list.toml")
+    pth_trsp=joinpath(pth,sol,"ECCO_transport_lines")
+end
 
 pth0=joinpath(pth,sol)
 !isdir(pth0) ? mkdir(pth0) : nothing
 
-@everywhere fil=joinpath(pth,sol,"ECCO_standard_list.toml")
 !isfile(fil) ? ECCO_helper_functions.standard_list_toml(fil) : nothing
 @everywhere list0=TOML.parsefile(joinpath(pth,sol,"ECCO_standard_list.toml"))
 
-@everywhere pth0=joinpath(pth,sol,"ECCO_transport_lines")
-!isdir(pth0) ? ECCO_helper_functions.transport_lines(pth0) : nothing
-@everywhere list_trsp,msk_trsp,ntr=ECCO_helper_functions.reload_transport_lines(pth0)
+!isdir(pth_trsp) ? ECCO_helper_functions.transport_lines(pth_trsp) : nothing
+@everywhere list_trsp,msk_trsp,ntr=ECCO_helper_functions.reload_transport_lines(pth_trsp)
 
-for ff in 1:length(list0["kk"])
+list1=collect(1:length(list0["kk"]))
+#zz=collect(1:6)
+#zz=[7,8,12,13]
+#zz=[25,26,27,28]
+
+for ff in zz
     save(joinpath(pth,sol,"taskID.jld2"),"ID",ff)
     
     @sync @everywhere gg=load(joinpath(pth,sol,"taskID.jld2"),"ID")
@@ -30,7 +44,7 @@ for ff in 1:length(list0["kk"])
     @everywhere pth_in,pth_out,pth_tmp,nt,list_steps=ECCO_helper_functions.path_etc(pth,sol0,calc,nam)
     !isdir(pth_tmp) ? mkdir(pth_tmp) : nothing
 
-    main_function(calc,sol,nam,kk)
+    ECCO_diagnostics.main_function(calc,sol,nam,kk; pth_in=pth_in, pth_out=pth_tmp)
 
 end
 
