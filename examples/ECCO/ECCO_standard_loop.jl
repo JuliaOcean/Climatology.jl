aac8950db549b8947e1cbfaed3fe1fa86c832d83
gaelforget <gforget@mit.edu>
2022-05-09 08:34:49 -0400

    doc updates

--- examples/ECCO/ECCO_standard_loop.jl
+++ examples/ECCO/ECCO_standard_loop.jl
@@ -3,7 +3,7 @@ using Distributed, OceanStateEstimation
 @everywhere begin
     using Pkg; Pkg.activate("./")
     
-    using OceanStateEstimation, Printf, TOML, MeshArrays
+    using OceanStateEstimation, Printf, MeshArrays
     using Distributed, SharedArrays, JLD2
 
     pth=joinpath(tempdir(),"ECCO_diags_dev")
@@ -16,14 +16,12 @@ using Distributed, OceanStateEstimation
 end
 
 !isdir(joinpath(pth,sol)) ? mkdir(joinpath(pth,sol)) : nothing
+@everywhere list0=ECCO_helpers.standard_list_toml("")
 
-!isfile(fil) ? ECCO_helpers.standard_list_toml(fil) : nothing
-@everywhere list0=TOML.parsefile(joinpath(pth,sol,"ECCO_standard_list.toml"))
-
-P0=ECCO_helpers.parameters(pth,sol0,list0,1)
+P0=ECCO_helpers.parameters(pth,sol0,list0[1])
 !isdir(pth_trsp) ? ECCO_helpers.transport_lines(P0.Γ,pth_trsp) : nothing
 
-list1=collect(1:length(list0["kk"]))
+list1=collect(1:length(list0))
 #list1=collect(1:6)
 #list1=[7,8,12,13]
 #list1=[25,26,27,28]
@@ -31,7 +29,7 @@ list1=collect(1:length(list0["kk"]))
 for ff in list1
 #    save(joinpath(pth,sol,"taskID.jld2"),"ID",ff)
 #    @sync @everywhere gg=load(joinpath(pth,sol,"taskID.jld2"),"ID")
-    P=ECCO_helpers.parameters(pth,sol0,list0,ff)
+    P=ECCO_helpers.parameters(pth,sol0,list0[ff])
     !isdir(P.pth_out) ? mkdir(P.pth_out) : nothing
     println("starting calc,sol,nam=$(P.calc),$(P.sol),$(P.nam) ...")
 
