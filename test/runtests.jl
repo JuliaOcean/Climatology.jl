6b04a914188be47bd1bbed891b8cd96acb950842
gaelforget <gforget@mit.edu>
2022-09-25 12:57:55 -0400

    add missing test

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,6 +24,7 @@ p=dirname(pathof(OceanStateEstimation))
 
     ##
 
+    OceanStateEstimation.MITPROFclim_download()
     OceanStateEstimation.CBIOMESclim_download()
     OceanStateEstimation.ECCOdiags_add("release2")
     OceanStateEstimation.ECCOdiags_add("interp_coeffs")
