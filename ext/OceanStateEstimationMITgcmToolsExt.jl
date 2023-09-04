fb4cf8ccd7c14050b8ffa400d017decf60ba95cb
gaelforget <gforget@mit.edu>
2023-09-04 01:42:30 -0400

    missing code in extension'

--- ext/OceanStateEstimationMITgcmToolsExt.jl
+++ ext/OceanStateEstimationMITgcmToolsExt.jl
@@ -1,8 +1,8 @@
 module OceanStateEstimationMITgcmToolsExt
 
-    import OceanStateEstimation: read_nctiles_alias 
+    import OceanStateEstimation: read_nctiles_alias, untargz_alias 
     
-    import MITgcmTools: read_nctiles
+    import MITgcmTools: read_nctiles, untargz
 
     read_nctiles_alias(args...;kwargs...)=read_nctiles(args...;kwargs...)
 
