08d56dffbea7555d1ffda0851e5b6511f13c7b50
gaelforget <gforget@mit.edu>
2024-09-10 07:28:29 -0400

    temporary fix in docs build

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,8 @@
-using Documenter, Climatology, MITgcm, MeshArrays, NCDatasets, Pkg
+using Documenter, Climatology
+import MITgcm, MeshArrays, NCDatasets, Pkg
 import PlutoSliderServer, CairoMakie
+import Climatology: Downloads
+
 Pkg.precompile()
 
 makedocs(;
@@ -20,7 +23,12 @@ makedocs(;
 Climatology.CBIOMESclim_download()
 Climatology.ECCOdiags_add("release2")
 Climatology.ECCOdiags_add("OCCA2HR1")
-withenv(interpolation_setup,"DATADEPS_ALWAYS_ACCEPT"=>true)
+withenv(MeshArrays.interpolation_setup,"DATADEPS_ALWAYS_ACCEPT"=>true)
+
+#temporary fix, which should no longer be needed after 0.5.9 is released
+earth_jpg=joinpath(tempdir(),"Blue_Marble.jpg")
+url="https://upload.wikimedia.org/wikipedia/commons/5/56/Blue_Marble_Next_Generation_%2B_topography_%2B_bathymetry.jpg"
+!isfile(earth_jpg) ? Downloads.download(url,earth_jpg) : nothing
 
 lst=("OISST/sst_anomaly_notebook.jl","CBIOMES/CBIOMES_climatology_plot.jl",
      "ECCO/ECCO_standard_plots.jl","NSLCT/NSLCT_notebook.jl",
